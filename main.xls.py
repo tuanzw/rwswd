565a9de89b79005aaacfbbfcc78e6017e5f64959
Nguyen <tuan.nguyen@rhenus.com>
2024-12-16 22:52:17 +0700

    save daily attendance report attachment

--- main.xls.py
+++ main.xls.py
@@ -2,14 +2,18 @@ import pandas as pd
 import argparse
 import os
 import csv
-import logging
 from datetime import datetime
 import paramiko
 import glob
 import shutil
+import win32com.client as win32
+import re
+from pathlib import PurePath
 from dotenv import dotenv_values
 
 from logapi import logger
+
+import pdb
 
 
 env = dotenv_values('.env')
@@ -64,8 +68,63 @@ def write_rows_to_csv(rows, filename):
         writer.writeheader()
         writer.writerows(rows)
 
+def save_mail_attachemnt(city_code: str):
+    outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
+    inbox = outlook.GetDefaultFolder(6)
+    # if env.get('email_folder') found, work on it else work on Inbox
+    working_folder = inbox
+    for folder in inbox.Folders:
+        if folder.name == env.get('email_folder'):
+            working_folder = folder
+            break
+    # Stop if there is no email_move_to_folder existed
+    try:
+        to_folder = working_folder.Folders(env.get('email_move_to_folder'))
+    except Exception as e:
+        print(
+            f"Please create {env.get('email_move_to_folder')} as subfolder of {working_folder.Name} in the mailbox"
+        )
+        logger(
+            f"Please create {env.get('email_move_to_folder')} as subfolder of {working_folder.Name} in the mailbox"
+        )
+        exit()
+
+    logger.info(f"Working on folder {working_folder.Name}")
+
+    # Sound goood, working on emails received in folder
+    # https://learn.microsoft.com/en-us/office/vba/outlook/how-to/search-and-filter/filtering-items-using-a-date-time-comparison
+    messages = working_folder.Items
+    to_move_messages = []
+    for message in messages:
+        mail_subject = message.Subject
+        # if not a email or subject pattern is not valid, skip
+        if message.Class != 43:
+            continue
+
+        if re.search(env.get('mail_subject_pattern'), mail_subject, re.IGNORECASE) is None:
+            msg = f"__SKIP__:{mail_subject}__Not well-formed Mail Subject"
+            logger.info(msg)
+            continue
+        if not mail_subject.split('_')[0].lower() == city_code:
+            msg = f"__SKIP__:{mail_subject}__{mail_subject.split('_')[0]} not a valid site"
+            logger.info(msg)
+            print(msg)
+            continue
+        # proceed to save .xlsx files in attachment of valid mail
+        to_move_messages.append(message)
+        attachments = message.Attachments
+        for attachment in attachments:
+            if attachment.FileName.split(".")[-1] == "xls":
+                file_path = PurePath(os.getcwd(), f'{city_code}_{attachment.FileName}')
+                attachment.SaveAsFile(file_path)
+    # all good, move proceeded email to email_move_to_folder
+    for message in to_move_messages:
+        message.Move(to_folder)
+        print(f"__PROCEEDED__:{message.Subject}")
+
 def extract_data_to_file(input_fn):
-    city_code, wdate, _ = fn.split('_')
+    city_code, rest = fn.split('_')
+    wdate = rest[16:26]
     wdate = datetime.strftime(datetime.strptime(wdate, '%Y-%m-%d'), '%Y%m%d')
     zk_df = pd.read_excel(input_fn, sheet_name=0, header=0,
                               converters={earliest_time: str, latest_time: str})
@@ -93,10 +152,13 @@ if __name__ == '__main__':
 
     # Parse the arguments
     args = parseArguments()
-    _csv = args.csv
+    _csv = args.csv.strip()
 
     try:
         logger.info('Start')
+
+        save_mail_attachemnt(city_code=_csv)
+
         input_filenames = glob.glob(f'{_csv}*.xls')
         for fn in input_filenames:
             extract_data_to_file(fn)
